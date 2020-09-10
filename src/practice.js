86c64137621ab342bc2a68a0102a73a6eee9c7af
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-09-10 13:14:25 -0400

    added knex instances for querying products by name and for displaying pages of products

--- src/practice.js
+++ src/practice.js
@@ -7,4 +7,29 @@ const knexInstance = knex({
   connection: process.env.DB_URL
 });
 
-console.log('knex and driver installed correctly');
+function searchByProductName(searchTerm) {
+  knexInstance
+  .from('amazong_products')
+  .select('product_id', 'name', 'price', 'category')
+  .where('name', 'ILIKE', `%${searchTerm}%`)
+  .first()
+  .then(result => {
+    console.log(result);
+  });
+}
+
+function paginateProducts(page) {
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1)
+  knexInstance
+    .from('amazong_products')
+    .select('product_id', 'name', 'price', 'category')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+//searchByProductName('holo');
+paginateProducts(2);
