2aae33b1bff560ebbfac90dddce8483f84055b95
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-09-13 14:47:43 -0400

    created tests and CRUD methods for the ShoppingListService object

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,13 +1,38 @@
 const ShoppingListService = {
-  // Get all items
-
-  // Get an item by ID
-
-  // Insert an item
-
-  // Update an item
-
-  // Delete an item
+  getAllItems(knex) {
+    return knex
+      .select('*')
+      .from('shopping_list');
+  },
+  getById(knex, itemId) {
+    return knex
+      .select('*')
+      .from('shopping_list')
+      .where(
+        'id',
+        '=',
+        itemId
+      );
+  },
+  deleteItem(knex, itemId) {
+    return knex
+      .del()
+      .from('shopping_list')
+      .where('id', itemId);
+  },
+  updateItem(knex, itemId, updatedContent) {
+    return knex
+      .update(updatedContent)
+      .from('shopping_list')
+      .where('id', itemId);
+  },
+  insertItem(knex, itemToInsert) {
+    return knex
+      .insert(itemToInsert)
+      .into('shopping_list')
+      .returning('*')
+      .then(result => result[0]);
+  }
 }
 
 module.exports = ShoppingListService;
--- test/shopping-list-service.test.js
+++ test/shopping-list-service.test.js
@@ -15,34 +15,34 @@ describe("ShoppingListService object", () => {
     {
       id: 1,
       name: "Fish tricks",
-      price: 13.1,
+      price: "13.10",
       date_added: new Date("2029-01-22T16:28:32.615Z"),
       checked: false,
-      category: Main,
+      category: "Main",
     },
     {
       id: 2,
       name: "Not Dogs",
-      price: 4.99,
+      price: "4.99",
       date_added: new Date("2029-01-22T16:28:32.615Z"),
       checked: true,
-      category: Snack,
+      category: "Snack",
     },
     {
       id: 3,
       name: "Tofurkey",
-      price: 2.5,
+      price: "2.50",
       date_added: new Date("2029-01-22T16:28:32.615Z"),
       checked: false,
-      category: Breakfast,
+      category: "Breakfast",
     },
     {
       id: 4,
       name: "Turnip the Beat",
-      price: 0.2,
+      price: "0.20",
       date_added: new Date("2029-01-22T16:28:32.615Z"),
       checked: true,
-      category: Lunch,
+      category: "Lunch",
     },
   ];
 
@@ -58,7 +58,7 @@ describe("ShoppingListService object", () => {
   before(() => db('shopping_list').truncate());
 
   //Disconnect from the knex-practice-test db after running all tests
-  after(() => db('shopping_list').destroy());
+  after(() => db.destroy());
 
   //Clear data from the test table after each test
   afterEach(() => db('shopping_list').truncate());
@@ -70,19 +70,92 @@ describe("ShoppingListService object", () => {
         .insert(testItems)
         .into('shopping_list');
     });
+    
+    // Item ID to use in tests retrieving, updating, or deleting a single item
+    const itemId = 1;
 
-    it(`getAllItems`, () => {});
+    it(`getAllItems() resolves all items from 'shopping_list'`, () => {
+      return ShoppingListService.getAllItems(db)
+        .then(actual => expect(actual).to.eql(testItems));
+    });
 
-    it(`getById()`, () => {});
+    it(`getById() resolves a single item object from 'shopping_list'`, () => {
+      return ShoppingListService.getById(db, itemId)
+        .then(actual => {
+          expect(actual[itemId - 1]).to.eql(testItems[itemId - 1]);
+        });
+    });
 
-    it(`deleteItem()`, () => {});
+    it(`deleteItem() should return 0 rows affected`, () => {
+      return ShoppingListService.deleteItem(db, 500)
+        .then(actual => {
+          expect(actual).to.eq(0);
+        });
+    });
 
-    it(`updateItem()`, () => {});
+    it(`deleteItem() deletes a single item by ID from 'shopping_list'`, () => {
+      // Simulate the SL post-deleteItem() since deleteItem() just returns a number
+      const expected = testItems.filter(item => item.id !== itemId);
+
+      // The first .then() gets and passes through the SL post-deleteItem; 
+      // the second .then() receives the mutated SL and makes the assertion
+      return ShoppingListService.deleteItem(db, itemId)
+        .then(() => ShoppingListService.getAllItems(db))
+        .then(actual => {
+          expect(actual).to.eql(expected);
+        });
+    });
+
+    it(`updateItem() should return 0 rows affected`, () => {
+      const updatedContent = {
+        name: "Gushers",
+        price: "5.00",
+        date_added: new Date("2029-01-22T16:28:32.615Z"),
+        checked: false,
+        category: "Snack",  
+      };
+      return ShoppingListService.updateItem(db, 500, updatedContent)
+        .then(actual => {
+          expect(actual).to.eq(0);
+        });
+    });
+
+    it(`updateItem() updates an existing item in 'shopping_list'`, () => {
+      const updatedContent = {
+        name: "Gushers",
+        price: "5.00",
+        date_added: new Date("2029-01-22T16:28:32.615Z"),
+        checked: false,
+        category: "Snack",  
+      }
+      return ShoppingListService.updateItem(db, itemId, updatedContent)
+        .then(() => ShoppingListService.getById(db, itemId))
+        .then(actual => {
+          expect(actual[itemId - 1]).to.eql({id: itemId, ...updatedContent});
+        });
+    });
   });
 
   context(`Given 'shopping_list has no data`, () => {
-    it(`getAllItems()`, () => {});
+    it(`getAllItems() resolves all items from 'shopping_list'`, () => {
+      return ShoppingListService.getAllItems(db)
+      .then(actual => expect(actual).to.eql([]));
+    });
 
-    it("insertItem()", () => {});
+    it("insertItem() inserts a new item and resolves the new article with an 'id'", () => {
+      const itemToInsert = {
+        id: 1,
+        name: "Gushers",
+        price: "5.00",
+        date_added: new Date("2029-01-22T16:28:32.615Z"),
+        checked: false,
+        category: "Snack",  
+      }
+  
+      return ShoppingListService.insertItem(db, itemToInsert)
+        .then(actual => {
+          expect(actual).to.eql(itemToInsert);
+        });      
+    });
   });
 });
